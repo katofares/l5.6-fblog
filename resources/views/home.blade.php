ad9ddf9440190df86e3721e047f2d5f05fc9e986
Fares Juma Kato <pshihashafares@gmail.com>
2018-06-04 07:43:31 +0300

    BE dashboard page modifications

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,78 +5,70 @@
 <div class="row">
     @php($userPostsCount = $currentUser->posts->count())
     @if($userPostsCount > 0)
-        <div class="col-sm-12 mb-4">
-            <div class="card-group">
-                <div class="card col-md-6 no-padding ">
-                    <div class="card-body">
-                        <div class="h1 text-muted text-right mb-4">
-                            <i class="fa fa-users"></i>
+        <div class="col-lg-3 col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="stat-widget-four">
+                        <div class="stat-icon dib">
+                            <i class="ti-server text-muted"></i>
                         </div>
-
-                        <div class="h4 mb-0">
-                            <span class="count">87500</span>
+                        <div class="stat-content">
+                            <div class="text-left dib">
+                                <div class="stat-heading">Posts</div>
+                                <div class="stat-text">Total: 765</div>
+                            </div>
                         </div>
-
-                        <small class="text-muted text-uppercase font-weight-bold">Visitors</small>
-                        <div class="progress progress-xs mt-3 mb-0 bg-flat-color-1" style="width: 40%; height: 5px;"></div>
                     </div>
                 </div>
-                <div class="card col-md-6 no-padding ">
-                    <div class="card-body">
-                        <div class="h1 text-muted text-right mb-4">
-                            <i class="fa fa-user-plus"></i>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="stat-widget-four">
+                        <div class="stat-icon dib">
+                            <i class="ti-user text-muted"></i>
                         </div>
-                        <div class="h4 mb-0">
-                            <span class="count">385</span>
+                        <div class="stat-content">
+                            <div class="text-left dib">
+                                <div class="stat-heading">Users</div>
+                                <div class="stat-text">Total: 24720</div>
+                            </div>
                         </div>
-                        <small class="text-muted text-uppercase font-weight-bold">New Clients</small>
-                        <div class="progress progress-xs mt-3 mb-0 bg-flat-color-2" style="width: 40%; height: 5px;"></div>
                     </div>
                 </div>
-                <div class="card col-md-6 no-padding ">
-                    <div class="card-body">
-                        <div class="h1 text-muted text-right mb-4">
-                            <i class="fa fa-cart-plus"></i>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="stat-widget-four">
+                        <div class="stat-icon dib">
+                            <i class="ti-stats-up text-muted"></i>
                         </div>
-                        <div class="h4 mb-0">
-                            <span class="count">1238</span>
+                        <div class="stat-content">
+                            <div class="text-left dib">
+                                <div class="stat-heading">Daily Sales</div>
+                                <div class="stat-text">Total: $76,58,714</div>
+                            </div>
                         </div>
-                        <small class="text-muted text-uppercase font-weight-bold">Products sold</small>
-                        <div class="progress progress-xs mt-3 mb-0 bg-flat-color-3" style="width: 40%; height: 5px;"></div>
                     </div>
                 </div>
-                <div class="card col-md-6 no-padding ">
-                    <div class="card-body">
-                        <div class="h1 text-muted text-right mb-4">
-                            <i class="fa fa-pie-chart"></i>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="stat-widget-four">
+                        <div class="stat-icon dib">
+                            <i class="ti-pulse text-muted"></i>
                         </div>
-                        <div class="h4 mb-0">
-                            <span class="count">28</span>%
+                        <div class="stat-content">
+                            <div class="text-left dib">
+                                <div class="stat-heading">Bandwidth</div>
+                                <div class="stat-text">Total: 4TB</div>
+                            </div>
                         </div>
-                        <small class="text-muted text-uppercase font-weight-bold">Returning Visitors</small>
-                        <div class="progress progress-xs mt-3 mb-0 bg-flat-color-4" style="width: 40%; height: 5px;"></div>
-                    </div>
-                </div>
-                <div class="card col-md-6 no-padding ">
-                    <div class="card-body">
-                        <div class="h1 text-muted text-right mb-4">
-                            <i class="fa fa-clock-o"></i>
-                        </div>
-                        <div class="h4 mb-0">5:34:11</div>
-                        <small class="text-muted text-uppercase font-weight-bold">Avg. Time</small>
-                        <div class="progress progress-xs mt-3 mb-0 bg-flat-color-5" style="width: 40%; height: 5px;"></div>
-                    </div>
-                </div>
-                <div class="card col-md-6 no-padding ">
-                    <div class="card-body">
-                        <div class="h1 text-muted text-right mb-4">
-                            <i class="fa fa-comments-o"></i>
-                        </div>
-                        <div class="h4 mb-0">
-                            <span class="count">972</span>
-                        </div>
-                        <small class="text-muted text-uppercase font-weight-bold">COMMENTS</small>
-                        <div class="progress progress-xs mt-3 mb-0 bg-flat-color-1" style="width: 40%; height: 5px;"></div>
                     </div>
                 </div>
             </div>
